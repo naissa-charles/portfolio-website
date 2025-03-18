feb637c7cf339a70268353c8d31484dba9856e71
Naissa Charles <naissacharles@Naissas-MacBook-Pro-2.local>
2025-03-17 20:31:43 -0500

    changing email back to original

--- src/app/action/sendEmail.ts
+++ src/app/action/sendEmail.ts
@@ -31,7 +31,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Portfolio Contact Form <onboarding@resend.dev>",
-      to: "naissacharles.dev@gmail.com",
+      to: "naissabcharles@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       //text: message as string,
